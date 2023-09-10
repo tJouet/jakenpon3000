6c9bd8c60a90b803e70d455be02a47a7b5d1dc23
ThibJouet <thibaut.jouet64@gmail.com>
2023-09-11 00:22:32 +0200

    Testing tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -4,24 +4,25 @@ import Image from "next/image";
 import {BannerCircleMenu} from "../public";
 import {Button, Frame, Input} from "@components/index";
 import {useRouter} from "next/navigation";
-
+//import styles from './styles/index.scss'
 
 const MainPage: React.FC = () => {
+
     const router = useRouter()
     const onclick = () => {
-        router.push('/about')
+        console.log('test')
+        router.push('/about/page')
     }
 
-    return <Frame className={'bg-primaryBlue p-40 h-screen flex flex-col justify-between items-center'}>
-        <Image src={BannerCircleMenu} alt={"Banner jakenpon"}/>
-        <Frame style={{flexDirection: 'row'}}>
-            <p className={'text-60 text-white'}>JAKENPON</p>
-            {/*Why does this font size doesn't work but the one in Text.module.css does?*/}
-            <p className={'text-3 pb-[55px]'}>3000</p>
-        </Frame>
+    return <Frame className={'bg-gradient-to-t from-primaryBlue via-secondaryBlue to-primaryBlue p-40 h-screen flex flex-col justify-between items-center'}>
+        <Image src={BannerCircleMenu} alt={"Banner jakenpon"} className={styles.imgSpin}/>
+        <div className={"flex flex-row"}>
+            <p className={'text-[60px] text-white drop-shadow-button' }>JAKENPON</p>
+            <p className={'text-[20px] text-white'}>3000</p>
+        </div>
         <Input placeholder="Set your nickname"/>
         <Button onClick={onclick}>
-            <p className={'text-60'}>Play!</p>
+            <p className={'text-60 drop-shadow-text text-secondaryBlue'}>Play!</p>
         </Button>
     </Frame>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primaryBlue: '#548CF3FF',
+        primaryBlue: '#044389',
+        secondaryBlue:"#215391",
         primaryYellow: '#FFE500',
       },
       boxShadow: {
-        button: 'inset 0px -5px 15px -1px rgba(0, 0, 0, 0.40)'
+        button: 'inset 0px -5px 15px -1px rgba(0, 0, 0, 0.40)',
       },
       fontSize: {
         '3': '3px',
         '60': '60px',
+      },
+      stroke:{
+        text: '2x black'
       },
       spacing: {
         '20': '20px',
