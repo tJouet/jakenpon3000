3cac7bbfc11ef8cc69fa2cfcf4b9a700db60be13
ThibJouet <thibaut.jouet64@gmail.com>
2023-09-13 17:53:06 +0200

    Added failsafe for username length.

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,13 @@ const MainPage: React.FC = () => {
     const router = useRouter()
 
     const onclick = () => {
-        gameContext.username = Username
-        router.push('/about')
+        if (Username.length < 3) {
+           return alert ("Your username must be 3 characters or bigger")
+        } else {
+            gameContext.username = Username
+            router.push('/play')
+        }
+        
     }
 
     const defineUsername = (e:React.FormEvent<HTMLInputElement>) => {
@@ -23,7 +28,7 @@ const MainPage: React.FC = () => {
     }
 
     return <Frame className={'bg-gradient-to-t from-primaryBlue via-secondaryBlue to-primaryBlue p-40 h-screen flex flex-col justify-between items-center'}>
-        <Image src={BannerCircleMenu} alt={"Banner jakenpon"}/>
+        <Image src={BannerCircleMenu} alt={"Banner jakenpon"} className={"animate-[spin_50s_linear_infinite]"}/>
         <div className={"flex flex-row"}>
             <p className={'text-[60px] text-white drop-shadow-button' }>JAKENPON</p>
             <p className={'text-[20px] text-white'}>3000</p>
--- app/play/page.tsx
+++ app/play/page.tsx
@@ -1,8 +1,20 @@
-import Link from "next/link";
+'use client'
+import React, {useContext} from "react";
+import GameContext from "@components/appContext";
+import { Frame } from "@components/index";
 
-export default function About() {
-    return <h2>
-        Hello from about
-        <Link href={'/about/coucou'} >GO TO Coucou</Link>
-    </h2>
+export default function Play() {
+
+    const gameContext = useContext(GameContext)
+
+    return(
+    <Frame className={'bg-gradient-to-t from-primaryBlue via-secondaryBlue to-primaryBlue p-40 h-screen flex flex-col justify-between items-center'}>
+        <div className={"flew-row flex text-white w-screen justify-center"}> 
+        <p>Awaiting</p>
+        <p className={"animation-delay-0 animate-[bounce_1s_infinite] "} >.</p>
+        <p className={"animation-delay-75 animate-[bounce_1s_infinite] "}>.</p>
+        <p className={"animation-delay-100 animate-[bounce_1s_infinite] " }>.</p>
+        </div>
+    </Frame>
+    )
 }
