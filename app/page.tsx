fc1cd4036e5050b3b8c8fa2ef6869b1e2578e792
ThibJouet <thibaut.jouet64@gmail.com>
2023-09-13 17:07:43 +0200

    minor clean

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ const MainPage: React.FC = () => {
     }
 
     return <Frame className={'bg-gradient-to-t from-primaryBlue via-secondaryBlue to-primaryBlue p-40 h-screen flex flex-col justify-between items-center'}>
-        <Image src={BannerCircleMenu} alt={"Banner jakenpon"} className={styles.imgSpin}/>
+        <Image src={BannerCircleMenu} alt={"Banner jakenpon"}/>
         <div className={"flex flex-row"}>
             <p className={'text-[60px] text-white drop-shadow-button' }>JAKENPON</p>
             <p className={'text-[20px] text-white'}>3000</p>
